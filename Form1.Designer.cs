254e399c5200dde85f437687e8c6cab07f197d08
hfjxjjd123 <63008138+hfjxjjd123@users.noreply.github.com>
2022-05-04 22:39:04 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace GiveMeTheMoney
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.상위값조회 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.계좌정보호출 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.구매창 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(997, 572);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(100, 50);
+            this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 74);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 52);
+            this.listBox1.TabIndex = 2;
+            // 
+            // 상위값조회
+            // 
+            this.상위값조회.Location = new System.Drawing.Point(27, 202);
+            this.상위값조회.Name = "상위값조회";
+            this.상위값조회.Size = new System.Drawing.Size(163, 70);
+            this.상위값조회.TabIndex = 8;
+            this.상위값조회.Text = "상위값조회";
+            this.상위값조회.UseVisualStyleBackColor = true;
+            this.상위값조회.Click += new System.EventHandler(this.상위값조회_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(227, 202);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(395, 400);
+            this.listBox2.TabIndex = 9;
+            // 
+            // 계좌정보호출
+            // 
+            this.계좌정보호출.Location = new System.Drawing.Point(303, 12);
+            this.계좌정보호출.Name = "계좌정보호출";
+            this.계좌정보호출.Size = new System.Drawing.Size(125, 50);
+            this.계좌정보호출.TabIndex = 10;
+            this.계좌정보호출.Text = "계좌정보호출";
+            this.계좌정보호출.UseVisualStyleBackColor = true;
+            this.계좌정보호출.Click += new System.EventHandler(this.계좌정보호출_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(979, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // 구매창
+            // 
+            this.구매창.FormattingEnabled = true;
+            this.구매창.ItemHeight = 12;
+            this.구매창.Location = new System.Drawing.Point(642, 202);
+            this.구매창.Name = "구매창";
+            this.구매창.Size = new System.Drawing.Size(336, 400);
+            this.구매창.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(979, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(979, 84);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 624);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.구매창);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.계좌정보호출);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.상위값조회);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Name = "Form1";
+            this.Text = "알바비충당";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button 상위값조회;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button 계좌정보호출;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox 구매창;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiveMeTheMoney
+{
+    public partial class Form1 : Form
+    {
+        List<List<string>> hotList = new List<List<string>>();
+        int[] time = { 30, 1, 2, 3, 4, 5, 6, 7 };
+        List<string> semiList = new List<string>(); // 여기 바꿨어요~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        List<string> buyList = new List<string>();
+        int count = 0;
+        int counter = 0;
+        string accountNo = "8020133911";
+        int depoeach;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            button1.PerformClick();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (axKHOpenAPI1.CommConnect() == 0)
+                listBox1.Items.Add("로그인시작");
+
+            else
+                listBox1.Items.Add("로그인 실패 : 접속이 안돼있습니다");
+
+            axKHOpenAPI1.OnEventConnect += onEventConnect;
+        }
+        //
+        public void onEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if (e.nErrCode == 0)
+            {
+                listBox1.Items.Add("로그인성공");
+                if (axKHOpenAPI1.GetConnectState() == 1)
+                    listBox1.Items.Add("접속상태:연결중");
+                else if (axKHOpenAPI1.GetConnectState() == 0)
+                    listBox1.Items.Add("접속상태:미연결");
+                계좌정보호출.PerformClick();
+            }
+        }
+        //
+        private void 계좌정보호출_Click(object sender, EventArgs e)
+        {
+            //계좌정보호출
+            
+            string accountPw = "0000";
+
+            axKHOpenAPI1.SetInputValue("계좌번호", accountNo);
+            axKHOpenAPI1.SetInputValue("비밀번호", accountPw);
+            axKHOpenAPI1.SetInputValue("비밀번호입력매체구분", "00");
+            axKHOpenAPI1.SetInputValue("조회구분", "2");
+
+            int acFind = axKHOpenAPI1.CommRqData("예수금상세현황요청", "opw00001", 0, "3001");
+
+            if (acFind == 0)
+                listBox1.Items.Add("예수금상세현황요청 성공");
+            else
+                listBox1.Items.Add("예수금상세현황요청 실패");
+            axKHOpenAPI1.OnReceiveTrData += onReceiveTrData예수금;
+
+
+        }
+        public void onReceiveTrData예수금(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            if (e.sRQName == "예수금상세현황요청")
+            {
+
+                string 예수금 = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, 0, "예수금").Trim();
+                // int deposit = int.Parse(예수금);
+                int canBuy = (int.Parse(예수금)) / 6;
+                listBox1.Items.Add("종목당: " + canBuy);
+                depoeach = canBuy;
+                상위값조회.PerformClick();
+            }
+        }
+        //
+        private void 상위값조회_Click(object sender, EventArgs e)
+        {
+            axKHOpenAPI1.SetInputValue("시장구분", "001"); axKHOpenAPI1.SetInputValue("정렬구분", "1"); axKHOpenAPI1.SetInputValue("거래량조건", "0050"); axKHOpenAPI1.SetInputValue("종목조건", "16"); axKHOpenAPI1.SetInputValue("상하한포함", "0");
+
+            int nFind = axKHOpenAPI1.CommRqData("전일대비등락률상위", "OPT10027", 0, "2001");
+            if (nFind == 0)
+                listBox1.Items.Add("전일대비등락률상위 성공");
+            else
+                listBox1.Items.Add("전일대비등락률상위 실패");
+            axKHOpenAPI1.OnReceiveTrData += onReceiveTrData상위값;
+        }
+        //
+        public void onReceiveTrData상위값(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            if (e.sRQName == "전일대비등락률상위")
+            {
+
+                for (int nIdx = 0; nIdx <= 4; nIdx++)
+                {
+                    string code = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "종목코드").Trim();
+                    string nowprice = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "현재가").Trim();
+
+                    listBox2.Items.Add(code);
+                    listBox2.Items.Add(nowprice);
+
+                    semiList.Add(code);
+                    semiList.Add(nowprice);
+
+                }
+                listBox2.Items.Add(" ");
+
+                Delay(10000);
+                button2.PerformClick();
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axKHOpenAPI1.SetInputValue("시장구분", "001"); axKHOpenAPI1.SetInputValue("정렬구분", "1"); axKHOpenAPI1.SetInputValue("거래량조건", "0050"); axKHOpenAPI1.SetInputValue("종목조건", "16"); axKHOpenAPI1.SetInputValue("상하한포함", "0");
+
+            int nFind = axKHOpenAPI1.CommRqData("전일대비등락률상위2","OPT10027", 0, "2001");
+            if (nFind == 0)
+                listBox1.Items.Add("전일대비등락률상위 성공");
+            else
+                listBox1.Items.Add("전일대비등락률상위 실패");
+            axKHOpenAPI1.OnReceiveTrData += onReceiveTrData상위값2;
+        }
+
+        public void onReceiveTrData상위값2(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            if (e.sRQName == "전일대비등락률상위2")
+            {
+                for (int nIdx = 0; nIdx <= 4; nIdx++)
+                {
+                    string code = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "종목코드").Trim();
+                    string nowprice = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "현재가").Trim();
+
+                    listBox2.Items.Add(code); 
+                    listBox2.Items.Add(nowprice); 
+                    bool previous = false;
+
+                    for (int cnt = 0; cnt<10; cnt=cnt+2)
+                    {
+                        if (code == semiList[cnt]) 
+                        {
+                            previous = true;
+                            if (nIdx * 2 < (cnt))
+                            {
+                                nowprice.Replace("+","");
+                                int value = int.Parse(nowprice);
+                                axKHOpenAPI1.SendOrder("주문", "4001", accountNo, 1, code, (depoeach / value ), 0, "03", ""); //구매요청
+                                구매창.Items.Add(code + "를 주문했습니다");
+                                if (buyList.Contains(code) == false) { buyList.Add(code); }
+                                axKHOpenAPI1.SendOrder("주문", "4002", accountNo, 2, code, (depoeach /value), (int)(value*1.03), "00", ""); //판매요청
+                                counter++;
+                            }
+                        } 
+                          
+                    }
+
+                    if (previous == false)
+                    {
+                        nowprice.Replace("+", "");
+                        int value = int.Parse(nowprice);
+                        axKHOpenAPI1.SendOrder("주문", "4001", accountNo, 1, code, (depoeach / value), 0, "03", ""); //구매요청
+                        구매창.Items.Add(code + "를 주문했습니다");
+                        if (buyList.Contains(code) == false) { buyList.Add(code); }
+                        axKHOpenAPI1.SendOrder("주문", "4002", accountNo, 2, code, (depoeach / value), (int)(value * 1.03), "00", ""); //판매요청
+                        counter++;
+                    }
+
+                    semiList.Add(code);
+                    semiList.Add(nowprice);
+             
+                }
+                listBox2.Items.Add(" ");
+                count = count + 10;
+                Delay(20000);
+                button3.PerformClick();
+
+
+            }
+        }
+
+
+        // axKHOpenAPI1.SendOrder("주문", "4001", accountNo, 1, code, quan, 0,"03", ""); //구매요청
+        //axKHOpenAPI1.SendOrder("주문", "4001", accountNo, 1, code, quan, 0, "03", ""); ;//구매요청
+
+
+        /*바로 매도 주문을 걸지 or 모든 매수주문을 마친 후 매도 주문을 걸지? 
+         후 매도주문이 더 나아보임 잠깐이나마 모든 주문이 체결될 때까지 기다리고 어차피 루프도는데 시간 별로 안걸려서
+        그사이에 +3퍼가 되는 종목은 없을것으로 판단
+
+        구매한 종목을 보이는 buyList에 종목정보 집어넣고 관리
+         */
+
+
+
+        //
+
+        public void Delay(int ms)
+        {
+            DateTime dateTimeNow = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, ms);
+            DateTime dateTimeAdd = dateTimeNow.Add(duration);
+            while (dateTimeAdd >= dateTimeNow)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                dateTimeNow = DateTime.Now;
+            }
+            return;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axKHOpenAPI1.SetInputValue("시장구분", "001");
+            axKHOpenAPI1.SetInputValue("정렬구분", "1");
+            axKHOpenAPI1.SetInputValue("거래량조건", "0050");
+            axKHOpenAPI1.SetInputValue("종목조건", "16");
+            axKHOpenAPI1.SetInputValue("상하한포함", "0");
+
+            int nFind = axKHOpenAPI1.CommRqData("전일대비등락률상위3", "OPT10027", 0, "2001");
+            if (nFind == 0)
+                listBox1.Items.Add("전일대비등락률상위 성공");
+            else
+                listBox1.Items.Add("전일대비등락률상위 실패");
+            axKHOpenAPI1.OnReceiveTrData += onReceiveTrData상위값3;
+        }
+
+        public void onReceiveTrData상위값3(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            if (e.sRQName == "전일대비등락률상위3")
+            {
+                for (int nIdx = 0; nIdx <= 4; nIdx++)
+                {
+                    string code = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "종목코드").Trim();
+                    string nowprice = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "현재가").Trim();
+
+
+                    listBox2.Items.Add(code);
+                    listBox2.Items.Add(nowprice);
+
+                    bool previous = false;
+                    if (buyList.Contains(code)==false) {
+                    
+                    for (int cnt = count; cnt < count+10; cnt = cnt + 2)
+                    {
+
+
+                        if (semiList[cnt] == code)
+                        {
+                            previous = true;
+                            if (nIdx * 2 < (cnt - count))
+                            {
+                                nowprice.Replace("+", "");
+                                int value = int.Parse(nowprice);
+                                axKHOpenAPI1.SendOrder("주문", "4001", accountNo, 1, code, (depoeach / value), 0, "03", ""); //구매요청
+                                구매창.Items.Add(code + "를 주문했습니다");
+                                if (buyList.Contains(code) == false) { buyList.Add(code); }
+                                axKHOpenAPI1.SendOrder("주문", "4002", accountNo, 2, code, (depoeach / value), (int)(value * 1.03), "00", ""); //판매요청
+                                counter++;
+
+                            }
+                        } 
+
+                    }
+                    if (previous == false)
+                    {
+                        nowprice.Replace("+", "");
+                        int value = int.Parse(nowprice);
+                        axKHOpenAPI1.SendOrder("주문", "4001", accountNo, 1, code, (depoeach / value), 0, "03", ""); //구매요청
+                        구매창.Items.Add(code + "를 주문했습니다");
+                        if (buyList.Contains(code) == false) { buyList.Add(code); }
+                        axKHOpenAPI1.SendOrder("주문", "4002", accountNo, 2, code, (depoeach / value), (int)(value * 1.03), "00", ""); //판매요청
+                        counter++;
+                    }
+                    }
+
+                    semiList.Add(code);
+                    semiList.Add(nowprice);
+
+                }
+                listBox2.Items.Add(" ");
+                count = count + 10;
+                Delay(20000);
+                button4.PerformClick();
+
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axKHOpenAPI1.SetInputValue("시장구분", "001");
+            axKHOpenAPI1.SetInputValue("정렬구분", "1");
+            axKHOpenAPI1.SetInputValue("거래량조건", "0050");
+            axKHOpenAPI1.SetInputValue("종목조건", "16");
+            axKHOpenAPI1.SetInputValue("상하한포함", "0");
+
+            int nFind = axKHOpenAPI1.CommRqData("전일대비등락률상위4", "OPT10027", 0, "2001");
+            if (nFind == 0)
+                listBox1.Items.Add("전일대비등락률상위 성공");
+            else
+                listBox1.Items.Add("전일대비등락률상위 실패");
+            axKHOpenAPI1.OnReceiveTrData += onReceiveTrData상위값4;
+        }
+
+        public void onReceiveTrData상위값4(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            if (e.sRQName == "전일대비등락률상위4")
+            {
+                for (int nIdx = 0; nIdx <= 4; nIdx++)
+                {
+                    string code = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "종목코드").Trim();
+                    string nowprice = axKHOpenAPI1.GetCommData(e.sTrCode, e.sRQName, nIdx, "현재가").Trim();
+
+
+                    listBox2.Items.Add(code);
+                    listBox2.Items.Add(nowprice);
+
+                    bool previous = false;
+                    if (buyList.Contains(code) == false)
+                    {
+
+                    for (int cnt = count; cnt < count + 10; cnt = cnt + 2)
+                    {
+
+
+                        if (semiList[cnt] == code)
+                            previous = true;
+                            if (nIdx * 2 < (cnt - count))
+                            {
+                                nowprice.Replace("+", "");
+                                int value = int.Parse(nowprice);
+                                axKHOpenAPI1.SendOrder("주문", "4001", accountNo, 1, code, (depoeach / value), 0, "03", ""); //구매요청
+                                구매창.Items.Add(code + "를 주문했습니다");
+                                buyList.Add(code); 
+                            axKHOpenAPI1.SendOrder("주문", "4002", accountNo, 2, code, (depoeach / value), (int)(value * 1.03), "00", ""); //판매요청
+                            counter++;
+                            } 
+
+                    }
+                    if (previous == false)
+                    {
+                        nowprice.Replace("+", "");
+                        int value = int.Parse(nowprice);
+                        axKHOpenAPI1.SendOrder("주문", "4001", accountNo, 1, code, (depoeach / value), 0, "03", ""); //구매요청
+                        구매창.Items.Add(code + "를 주문했습니다");
+                        buyList.Add(code);
+                        axKHOpenAPI1.SendOrder("주문", "4002", accountNo, 2, code, (depoeach /value), (int)(value*1.03), "00", ""); //판매요청
+                        counter++;
+                    }
+                    }
+
+                    semiList.Add(code);
+                    semiList.Add(nowprice);
+                }
+                listBox2.Items.Add(" ");
+                count = count + 10;
+
+
+            }
+        }
+    }
+}
