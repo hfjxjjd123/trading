425af0016197d7cc9b81f837c661b7d0f43d6d83
hfjxjjd123 <63008138+hfjxjjd123@users.noreply.github.com>
2022-05-10 07:31:41 +0900

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,7 +39,6 @@ namespace GiveMeTheMoney
             this.button2 = new System.Windows.Forms.Button();
             this.구매창 = new System.Windows.Forms.ListBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -129,22 +128,11 @@ namespace GiveMeTheMoney
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(979, 84);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 14;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1095, 624);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.구매창);
             this.Controls.Add(this.button2);
@@ -173,7 +161,6 @@ namespace GiveMeTheMoney
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox 구매창;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
 
